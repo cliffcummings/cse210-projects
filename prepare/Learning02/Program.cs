b8f66e877d996b7043ddc17fa9e70c560ddee9e6
cliffcummings <cliffc@sunburst-design.com>
2024-04-27 14:59:37 -0600

    Laerning02 Prepare coding assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+        
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear   = 2022;
+
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear   = 2023;
+
+        // Console.WriteLine($"{job1._company}");
+        // Console.WriteLine($"{job2._company}");
+
+        // job1.DisplayJobDetails();
+        // job2.DisplayJobDetails();
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Console.WriteLine($"{myResume._jobs[0]._jobTitle}");
+
+        myResume.DisplayJobDetails();
+
     }
 }
